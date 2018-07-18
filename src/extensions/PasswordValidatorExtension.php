0bf0f56e348c5663956c7a9e7ded4a3cb4444464
Simon Erkelens <simonerkelens@silverstripe.com>
2018-07-18 20:09:30 +1200

    Bughunting on CircleCI

--- src/extensions/PasswordValidatorExtension.php
+++ src/extensions/PasswordValidatorExtension.php
@@ -25,6 +25,12 @@ class PasswordValidatorExtension extends Extension
      */
     protected $service;
 
+    private static $allow_pwnd = false;
+
+    private static $pwn_treshold = 1;
+
+    private static $save_pwnd = true;
+
     /**
      * @param string $pwd
      * @param Member $member
@@ -37,22 +43,26 @@ class PasswordValidatorExtension extends Extension
     {
         $this->service = Injector::inst()->createWithArgs(HaveIBeenPwndService::class, [$params]);
 
-        $allowBreach = static::config()->get('allow_pwnd');
-        $breachCount = static::config()->get('pwn_treshold');
-        $storeBreach = static::config()->get('save_pwnd');
+        $allowPwnd = static::config()->get('allow_pwnd');
+        $pwnTreshold = static::config()->get('pwn_treshold');
+        $savePwnd = static::config()->get('save_pwnd');
         $isPwndCount = 0;
         $breached = '';
 
         // There's no need to check if breaches are allowed, it's a pointless excercise
-        if (!$allowBreach || $breachCount !== 0) {
+        if (!$allowPwnd || $pwnTreshold !== 0) {
             $isPwndCount = $this->service->checkPwndPassword($pwd);
         }
 
+
+        Debug::dump($allowPwnd);
+        Debug::dump($pwnTreshold);
+        Debug::dump($savePwnd);
         // Always mark as Pwnd if it's true
         $member->PasswordIsPwnd = $isPwndCount;
 
         // If storing the breached sites, check the email as well
-        if ($storeBreach) {
+        if ($savePwnd) {
             usleep(1500); // We need to conform to the FUP, max 1 request per 1500ms
             $breached = $this->service->checkPwndEmail($member);
             $member->BreachedSites = $breached;
@@ -60,7 +70,7 @@ class PasswordValidatorExtension extends Extension
 
         // Although it would be stupid, the pwnd treshold can be disabled
         // Or even allow for breached passwords. Not exactly ideal either
-        if (($isPwndCount >= $breachCount && $breachCount !== 0) || (!$allowBreach && $isPwndCount)) {
+        if (($isPwndCount >= $pwnTreshold && $pwnTreshold !== 0) || (!$allowPwnd && $isPwndCount)) {
             $valid->addFieldError(
                 'Password',
                 _t(static::class . 'KNOWN', 'Your password appears in the Have I Been Pwnd database')
